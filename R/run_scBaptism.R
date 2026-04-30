2a8c320f5b79efb21dff879d5ce68e43d7865aef
Alina Jenn <alina.jenn@web.de>
2026-04-30 16:24:00 +0200

    handling of parameters of all annotation methods implemented, users can now pass down any parameter from these tools

--- R/run_scBaptism.R
+++ R/run_scBaptism.R
@@ -7,7 +7,7 @@
 #' @param ref_labs name of column with annotation information
 #' @param return_extra_info if TRUE, adds additional metadata from the annotation when available
 #' @param verbose display message after each annotation is finished
-#'
+#' @param ... further arguments passed to other methods
 #'
 #' @returns sce_query : a SingleCellExperiment object, with the extra info on the
 #' annotated cells
@@ -23,67 +23,129 @@ run_scBaptism <- function(sce_query = NULL,
                           ref_labs = NULL,
                           verbose = FALSE,
                           return_extra_info = FALSE,
-                          ...)
-
-  {
+                          ...){
 
   # checks -------------------------------------------------------------
 
-  # checks of input parameters
+  # checks of each input parameter
 
   .check_query(sce_query)
   anno_methods <- .check_anno_methods(anno_methods)
   .check_markers_list(markers_list)
   .check_reference(reference, ref_labs)
 
-  #check is selected tools match provided inputs
 
-  anno_methods_final <- anno_methods
+  #check if selected tools match the  provided inputs
 
-  anno_referencebased <- c("SingleR", "Seurat", "clustifyr", "scPred", "scClassify", "scmap", "CelliDref")
-  anno_markerbased <- c("CIA", "SCINA", "CelliDmk")
+  anno_methods_final <- .match_input_methods(anno_methods)
 
-  #check if a reference is provided, when user selects reference-based tools
+  # prepare parameter inputs ---------------------------------------------------
 
-  if(is.null(reference) & any(anno_methods_final %in% anno_referencebased)) {
-    warning("No reference provided. Annotation will be performed with selected marker-based methods only")
-    anno_methods_final <- setdiff(anno_methods_final, anno_referencebased)
-    anno_methods_excluded <- setdiff(anno_referencebased, anno_methods_final)
-    if(length(anno_methods_final) == 0) {
-      stop("Please provide a reference")
-    }
-  }
+  # get all arguments users might have passed through the ...
 
-  #check if markers list is provided, when user selects marker-based tools
-  if(is.null(markers_list) & any(anno_methods_final %in% anno_markerbased)) {
-    warning("No markers list provided. and you wanted any marker based methods")
+  dots <- list(...)
 
-    anno_methods_final <- setdiff(anno_methods_final, anno_markerbased)
-    if(length(anno_methods_final) == 0) {
-      stop("Please provide a list of markers")
-    }
-  }
+
+  # get the extra arguments sorted into lists to pass them to the correct function
 
 
 
-  # run selected tools -------------------------------------------------
+  args_CIA <- dots[names(dots) %in% names(formals(run_CIA))]
+  args_SCINA <- dots[names(dots) %in% names(formals(run_SCINA))]
+  args_CelliDmk <- dots[names(dots) %in% names(formals(run_CelliDmk))]
+  args_SingleR <- dots[names(dots) %in% names(formals(run_SingleR))]
+  args_Seurat <- dots[names(dots) %in% names(formals(run_Seurat))]
+  args_clustifyr <- dots[names(dots) %in% names(formals(run_clustifyr))]
+  args_scPred <- dots[names(dots) %in% names(formals(run_scPred))]
+  args_scClassify <- dots[names(dots) %in% names(formals(run_scClassify))]
+  args_scmap <- dots[names(dots) %in% names(formals(run_scmap))]
+  args_CelliDref <- dots[names(dots) %in% names(formals(run_CelliDref))]
+
+
+
+  # run selected tools ------------------------------------------------------
   for (item in anno_methods_final) {
 
+    sce_query <- switch(EXPR = item,
+    'CIA' = sce_query <- do.call(run_CIA,
+                                 c(list(sce_query = sce_query,
+                                        markers_list = markers_list,
+                                        verbose = verbose),
+                                   args_CIA)),
 
-  sce_query <- switch(item,
-  'CIA' = sce_query <- run_CIA(sce_query = sce_query, markers_list = markers_list),
-  'SCINA' = sce_query <- run_SCINA(sce_query = sce_query, markers_list = markers_list),
-  'CelliDmk' = sce_query <- run_CelliDmk(sce_query = sce_query, markers_list = markers_list),
-  'SingleR' = sce_query <- run_SingleR(sce_query = sce_query, reference = reference, ref_labs = ref_labs),
-  'Seurat' = sce_query <- run_Seurat(sce_query = sce_query, reference = reference, ref_labs = ref_labs),
-  'clustifyr' = sce_query <- run_clustifyr(sce_query = sce_query, reference = reference, ref_labs = ref_labs),
-  'scPred' = sce_query <- run_scPred(sce_query = sce_query, reference = reference, ref_labs = ref_labs),
-  'scClassify' = sce_query <- run_scClassify(sce_query = sce_query, reference = reference, ref_labs = ref_labs),
-  'scmap' = sce_query <- run_scmap(sce_query = sce_query, reference = reference, ref_labs = ref_labs),
-  'CelliDref' = sce_query <- run_CelliDref(sce_query = sce_query, reference = reference, ref_labs = ref_labs)
-  )
+    'SCINA' = sce_query <- do.call(run_SCINA,
+                                   c(list(sce_query = sce_query,
+                                          markers_list = markers_list,
+                                          verbose = verbose,
+                                          return_extra_info = return_extra_info),
+                                     args_SCINA)),
+
+    'CelliDmk' = sce_query <- do.call(run_CelliDmk,
+                                      c(list(sce_query = sce_query,
+                                             markers_list = markers_list,
+                                             verbose = verbose,
+                                             return_extra_info = return_extra_info),
+                                        args_CelliDmk)),
+
+    'SingleR' = sce_query <- do.call(run_SingleR,
+                                     c(list(sce_query = sce_query,
+                                            reference = reference,
+                                            ref_labs = ref_labs,
+                                            verbose = verbose,
+                                            return_extra_info = return_extra_info),
+                                       args_SingleR)),
+
+    'Seurat' = sce_query <- do.call(run_Seurat,
+                                    c(list(sce_query = sce_query,
+                                           reference = reference,
+                                           ref_labs = ref_labs,
+                                           verbose = verbose,
+                                           return_extra_info = return_extra_info),
+                                      args_Seurat)),
+
+    'clustifyr' = sce_query <- do.call(run_clustifyr,
+                                       c(list(sce_query = sce_query,
+                                              reference = reference,
+                                              ref_labs = ref_labs,
+                                              verbose = verbose),
+                                         args_clustifyr)),
+
+    'scPred' = sce_query <- do.call(run_scPred,
+                                    c(list(sce_query = sce_query,
+                                           reference = reference,
+                                           ref_labs = ref_labs,
+                                           verbose = verbose,
+                                           return_extra_info = return_extra_info),
+                                      args_scPred)),
+
+    'scClassify' = sce_query <- do.call(run_scClassify,
+                                        c(list(sce_query = sce_query,
+                                               reference = reference,
+                                               ref_labs = ref_labs,
+                                               verbose = verbose,
+                                               return_extra_info = return_extra_info),
+                                          args_scClassify)),
+
+    'scmap' = sce_query <- do.call(run_scmap,
+                                   c(list(sce_query = sce_query,
+                                          reference = reference,
+                                          ref_labs = ref_labs,
+                                          verbose = verbose,
+                                          return_extra_info = return_extra_info),
+                                     args_scmap)),
+
+    'CelliDref' = sce_query <- do.call(run_CelliDref,
+                                       c(list(sce_query = sce_query,
+                                              reference = reference,
+                                              ref_labs = ref_labs,
+                                              verbose = verbose,
+                                              return_extra_info = return_extra_info),
+                                         args_CelliDref))
+    )
 
   }
+
+  # return result --------------------------------------------------------------------
 
   return(sce_query)
 }
