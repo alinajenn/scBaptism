50b2906e429d6ead57b6379f8494a32703833a21
Alina Jenn <alina.jenn@web.de>
2026-05-18 22:38:55 +0200

    finished implementation of calculate_metacells function

--- R/calculate_metacells.R
+++ R/calculate_metacells.R
@@ -1,7 +1,9 @@
 # metacellify function
 # make MCs before running any annotation
 
-calculate_metacells <- function(sce_query){  ##give options for parameter customization (gamma, mode)
+calculate_metacells <- function(sce_query,
+                                gamma = 10 #SCimplify graining level
+                                ){  ##give options for parameter customization (gamma, mode)
 
   #get the log normalized expression matrix
 
@@ -9,63 +11,36 @@ calculate_metacells <- function(sce_query){  ##give options for parameter custom
 
   #run the metacell calculations, metacell identity for each single cell
 
-  mc_identity <- SuperCell::SCimplify(X = query_matrix)
+  mc_identity <- SuperCell::SCimplify(X = query_matrix,
+                                      gamma = gamma)
 
-  #get average gene expression for each metacell
+  #get average gene expression for each metacell, in matrix format
 
   mc_matrix <- SuperCell::supercell_GE(ge = query_matrix,
-                                       groups = mc_identity$membership)
+                                       groups = mc_identity$membership,
+                                       mode = "average")
 
-  #create new SCE
-  ############could be replaced with supercell_2_sce
 
+  #create new SCE with the metacell expression matrix
   sce_metacells <- SingleCellExperiment::SingleCellExperiment(assays = list(logcounts = mc_matrix))
 
-  #calculate logcounts, does this make sense or is it doppelt-gemoppelt?
-  #Problem: I need counts & logcounts in order to run_scBaptism
-
-  #sce_metacells <- scuttle::logNormCounts(sce_metacells)
-
-  #add cellnames (form scClassify)
-  colnames(sce_metacells) <- paste0("cell_", seq_len(ncol(sce_metacells)))
-
-  #clusters (for clustifyr)
-  library(scran)
-
-  g <- buildSNNGraph(sce_metacells, k=10, use.dimred = 'PCA')
-  clust <- igraph::cluster_walktrap(g)$membership
-  colLabels(sce_metacells) <- factor(clust)
-
-  names(SummarizedExperiment::colData(sce_metacells))[which(names(SummarizedExperiment::colData(sce_metacells))=="label")]="clusters"
+  #add cellnames (needed for running scClassify)
+  colnames(sce_metacells) <- paste0("cell_", seq_len(SummarizedExperiment::ncol(sce_metacells)))
 
   #run PCA
   sce_metacells <- scater::runPCA(sce_metacells)
 
-  #calcuate tSNE (for plotting later)
-  ##################possibly add other dim reductions
+  #calculate clusters (needed for running clustifyr)
+  graph <- scran::buildSNNGraph(sce_metacells, k=10, use.dimred = 'PCA')
+  clust <- igraph::cluster_walktrap(graph)$membership
+  SingleCellExperiment::colLabels(sce_metacells) <- factor(clust)
+
+  names(SummarizedExperiment::colData(sce_metacells))[which(names(SummarizedExperiment::colData(sce_metacells))=="label")]="clusters"
+
+
+  #calcuate tSNE (useful for plotting)
   sce_metacells <- scater::runTSNE(sce_metacells)
 
   return(sce_metacells)
 
-  ####################add in cell names?
-
 }
-
-#Leas example
-
-# sce_counts <- assay(sce, "logcounts", cell.annotation = sce$labels_main)
-#
-# super <- SCimplify(sce_counts, gamma = 20)
-#
-# super_counts <- supercell_GE(sce_counts, super$membership, mode="average")
-#
-# sce_super <- supercell_2_sce(
-#   SC.GE = super_counts,
-#   SC = super
-# )
-
-
-######################old
-#add information back to the query SCE
-#SummarizedExperiment::colData(sce_query)$metacells <- MC_identity$membership
-#return(sce_query)
